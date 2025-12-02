a048f54a22400cb78da565965d53f33b2359202b
AftrLite <61218133+AftrLite@users.noreply.github.com>
2025-12-02 18:33:53 +1300

    initial commit

---
+++ Content.Stellar.Client/_ES/Chat/Obfuscation/ESVoiceObfuscatorSystem.cs
@@ -0,0 +1,6 @@
+using Content.Stellar.Shared._ES.Chat.Obfuscation;
+
+namespace Content.Stellar.Client._ES.Chat.Obfuscation;
+
+/// <inheritdoc/>
+public sealed class ESVoiceObfuscatorSystem : ESSharedVoiceObfuscatorSystem;
