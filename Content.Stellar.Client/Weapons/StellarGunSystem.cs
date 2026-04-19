75385ee5ee9bcf5d557f4d4914f9a33fec600e90
Janet Blackquill <uhhadd@gmail.com>
2026-04-19 00:51:30 -0400

    warnungs

--- Content.Stellar.Client/Weapons/StellarGunSystem.cs
+++ Content.Stellar.Client/Weapons/StellarGunSystem.cs
@@ -62,7 +62,7 @@ public sealed partial class StellarGunSystem : SharedStellarGunSystem
             return false;
 
         var gunEnt = _hands.GetActiveItem(player);
-        if (gunEnt is null || !EntityManager.TryGetComponent<StellarGunReloadableComponent>(gunEnt.Value, out var gunComp))
+        if (gunEnt is null || !TryComp<StellarGunReloadableComponent>(gunEnt.Value, out var gunComp))
             return false;
 
         if (!DoAfter.IsRunning(gunComp.ReloadDoAfter) && gunComp.AmmoReserves > 0 && gunComp.AmmoCount < gunComp.AmmoMagCapacity)
@@ -519,9 +519,9 @@ public sealed partial class StellarGunSystem : SharedStellarGunSystem
 
     private void SendReloadMessage(EntityUid gun, EntityUid player)
     {
-        var gunEnt = EntityManager.GetNetEntity(gun);
-        var playerEnt = EntityManager.GetNetEntity(player);
-        EntityManager.RaisePredictiveEvent(new StellarManualReloadEvent(gunEnt, playerEnt));
+        var gunEnt = GetNetEntity(gun);
+        var playerEnt = GetNetEntity(player);
+        RaisePredictiveEvent(new StellarManualReloadEvent(gunEnt, playerEnt));
     }
 }
 
